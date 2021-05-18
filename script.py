395e08c63218abf30f65176207a754c7f13eb55d
ԹՅɿՏɧɿkԵɧ ʝԾՏ <73195851+Hillard-har@users.noreply.github.com>
2021-05-18 13:02:08 +0530

    Small changes

---
+++ script.py
@@ -0,0 +1,47 @@
+class script(object):
+
+
+    START_TEXT = """** Hai {}** , 
+
+I am a Simple File Renamer Bot with  Permanent Thumbnail support
+
+Send me any Telegram file and choose appropriate option!
+
+"""
+
+    RENAME_403_ERR = "What Are You Doing? You are Banned"
+
+    UPGRADE_TEXT = "CONTACT @Ts_Bots"
+
+    DOWNLOAD_START = "Download Started... "
+
+    UPLOAD_START = "Uploading Started... "
+
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me > ©  @Ts_Bots**"
+
+    SAVED_THUMB = "Thumbnail Saved ✅ This Is Permanent"
+
+    DEL_THUMB = "Thumbnail cleared succesfully!"
+
+    NO_THUMB = "No thumbnails found!"
+
+    SAVED_RECVD_DOC_FILE = "File Downloaded Successfully 😎"
+
+    CUSTOM_CAPTION_UL_FILE = " "
+
+    HELP_USER = """
+
+<b>It's not that complicated😅</b>
+    
+1. Send me a custom thumbnail (Optional) 
+
+2. Send the file to be renamed
+
+3. Choose appropriate option
+
+4. Send the new file when bit asks 
+
+
+By @Ts_Bots
+"""
+
